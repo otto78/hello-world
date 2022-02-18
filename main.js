4ed8d0b5b5fdb418088ba53e90fd5d52205b9a12
Andrea Mazzarotto <andrea.mazzarotto@gmail.com>
2022-02-18 17:57:42 +0100

    responsive iniziato

--- main.js
+++ main.js
@@ -1,2 +1,24 @@
 
 
+width = document.documentElement.clientWidth
+console.log(width)
+
+w =document.documentElement.scrollWidth
+console.log(w)
+
+window.onresize = function(){
+    console.log(window.innerWidth)
+}
+
+let heightOutput = document.querySelector('#height');
+let widthOutput = document.querySelector('#width');
+
+function reportWindowSize() {
+  heightOutput = window.innerHeight;
+  widthOutput = window.innerWidth;
+}
+
+window.onresize = reportWindowSize;
+
+
+console.log(widthOutput)
