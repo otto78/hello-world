222e8a19a35aae224ca786f360639d391c620618
Andrea Mazzarotto <andrea.mazzarotto@gmail.com>
2022-02-19 11:00:45 +0100

    responsive ok

--- main.js
+++ main.js
@@ -1,24 +1,2 @@
 
 
-width = document.documentElement.clientWidth
-console.log(width)
-
-w =document.documentElement.scrollWidth
-console.log(w)
-
-window.onresize = function(){
-    console.log(window.innerWidth)
-}
-
-let heightOutput = document.querySelector('#height');
-let widthOutput = document.querySelector('#width');
-
-function reportWindowSize() {
-  heightOutput = window.innerHeight;
-  widthOutput = window.innerWidth;
-}
-
-window.onresize = reportWindowSize;
-
-
-console.log(widthOutput)
