239ad8fd7cff837f1912ef6ee97b64867cd5adaa
Andrea Mazzarotto <andrea.mazzarotto@gmail.com>
2022-02-19 17:04:51 +0100

    bio fatta

--- main.js
+++ main.js
@@ -1,3 +1,63 @@
 let freccia = document.querySelector('#freccia');
-freccia.addEventListener('mouseleave')
+let img = document.querySelector('#img')
+let dev = document.querySelector('#dev')
+
+freccia.addEventListener('click',()=>{
+    
+    img.style.filter = "brightness(20%)";
+    img.style.transition = "1s";
+    
+    
+    if(window.innerWidth<767){
+        freccia.style.transform = "rotate(-45deg)"
+    }else{
+    freccia.style.transform = "rotate(-135deg)"
+    }
+
+    dev.style.transform = "translateX(0%)"
+    dev.style.transition = "1s";
+
+    let col2 = document.querySelector('#col2');
+    let par = document.createElement('p');
+
+    par.classList.add('par');
+    if(window.innerWidth>=1200){
+        par.innerHTML =`
+        Sono uno sviluppatore<br> <strong>Web Full-Stack</strong>. Conosco i linguaggi di<br> programmazione <strong>HTML, CSS, JS, PHP</strong>, Framework <strong>Bootstrap</strong> e <strong>Laravel</strong> e sono formato sulla metodologia <strong>Agile</strong> e <strong>Scrum</strong>.
+        Sono appassionato di <strong>Grafica</strong> e orientato al <strong>Frontend</strong>. Arrivo dal corso Full-Stack Web Developer<br> di <strong>Aulab Hackademy</strong> e da studi universitari di <strong>Arte, Cinema e Teatro</strong>.
+        Sono una persona artistica dalle spiccate doti relazionali e sempre alla ricerca<br> di <strong>nuove sfide</strong>. Aspiro al costante<br> miglioramento di me stesso. 
+        `;
+    col2.append(par);
+    }
+    else if(window.innerWidth<1200 && window.innerWidth>768){
+    par.innerHTML =`
+    Sono uno sviluppatore<br> <strong>Web Full-Stack</strong>. Conosco i linguaggi di<br> programmazione <strong>HTML, CSS, JS, PHP</strong>, Framework <strong>Bootstrap</strong> e <strong>Laravel</strong> e sono formato sulla metodologia <strong>Agile</strong> e <strong>Scrum</strong>.
+    Sono appassionato di <strong>Grafica</strong> e orientato<br> al <strong>Frontend</strong>. Arrivo dal corso Full-Stack Web Developer<br> di <strong>Aulab Hackademy</strong> e da studi universitari di <strong>Arte, Cinema e Teatro</strong>.
+    Sono una persona artistica<br> dalle spiccate doti relazionali e sempre alla ricerca<br> di <strong>nuove sfide</strong>. Aspiro al costante<br> miglioramento di me stesso. 
+    `;
+    col2.append(par);}
+    else if(window.innerWidth<767){
+        par.innerHTML =`
+        Sono uno sviluppatore<br> <strong>Web Full-Stack</strong>. Conosco i linguaggi di<br> programmazione <strong>HTML, CSS, JS, PHP</strong>, Framework <br><strong>Bootstrap</strong> e <strong>Laravel</strong> e sono formato sulla metodologia <br><strong>Agile</strong> e <strong>Scrum</strong>.
+        Sono appassionato di <strong>Grafica</strong> e orientato<br> al <strong>Frontend</strong>. Arrivo dal corso Full-Stack Web Developer<br> di <strong>Aulab Hackademy</strong> e da studi universitari di <br><strong>Arte, Cinema e Teatro</strong>.
+        Sono una persona artistica<br> dalle spiccate doti relazionali e sempre alla ricerca<br> di <strong>nuove sfide</strong>. Aspiro al costante<br> miglioramento di me stesso. 
+        `;
+        col2.append(par);}
+    
+    
+        
+ 
+    
+ 
+ 
+ 
+ 
+    }
+
+    
+
+
+)
+
+
 
